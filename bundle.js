b6a4dade6d9f0e0a932624f5e0bc0a626e5bde9e
Dmitry <cybirgpl@gmail.com>
2022-03-17 19:15:33 +0700

    Написал документацию; добавил тест с созданием кастомного валидатора

---
+++ bundle.js
@@ -0,0 +1,9 @@
+const { FormValidator } = require( './libs/form/formValidator.js' );
+const { FormManipulator } = require( './libs/form/formManipulator.js' );
+const { VirtualFormFactory } = require( './libs/form/virtualFormFactory' );
+
+module.exports = {
+  FormManipulator,
+  FormValidator,
+  VirtualFormFactory
+};
